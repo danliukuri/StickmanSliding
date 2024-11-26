97c245c3ef48e6448cc8ab07d2b970934be629c8
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2024-11-26 16:10:14 +0200

    Create scene loading service

---
+++ StickmanSliding.Unity/Assets/Scripts/Runtime/Infrastructure/AssetLoading/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using Cysharp.Threading.Tasks;
+using StickmanSliding.Data.Static.Enumerations;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace StickmanSliding.Infrastructure.AssetLoading
+{
+    public class SceneLoader : ISceneLoader
+    {
+        public AsyncOperationHandle<SceneInstance> LoadingOperation { get; private set; }
+
+        public async UniTask<Scene> Load(SceneName sceneName, LoadSceneMode mode = LoadSceneMode.Single)
+        {
+            LoadingOperation = Addressables.LoadSceneAsync(sceneName.ToString(), mode, activateOnLoad: false);
+            await LoadingOperation.ToUniTask();
+            return LoadingOperation.Result.Scene;
+        }
+
+        public async UniTask<Scene> ActivateLastLoaded()
+        {
+            if (LoadingOperation.IsDone)
+                await LoadingOperation.Result.ActivateAsync().ToUniTask();
+            return LoadingOperation.Result.Scene;
+        }
+    }
+}
---
+++ StickmanSliding.Unity/Assets/Scripts/Runtime/Infrastructure/DependencyInjection/ProjectContext/GlobalServicesBindingsInstaller.cs
@@ -0,0 +1,12 @@
+﻿using StickmanSliding.Infrastructure.AssetLoading;
+using Zenject;
+
+namespace StickmanSliding.Infrastructure.DependencyInjection.ProjectContext
+{
+    public class GlobalServicesBindingsInstaller : MonoInstaller
+    {
+        public override void InstallBindings() => BindSceneLoader();
+
+        private void BindSceneLoader() => Container.BindInterfacesTo<SceneLoader>().AsSingle();
+    }
+}
