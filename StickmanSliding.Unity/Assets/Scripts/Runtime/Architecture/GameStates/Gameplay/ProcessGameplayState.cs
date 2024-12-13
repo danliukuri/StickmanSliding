f9c93651cd1a337349097210ca39d5ccd326864c
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2024-12-13 10:00:05 +0200

    Make camera follow player during gameplay

--- StickmanSliding.Unity/Assets/Scripts/Runtime/Architecture/GameStates/Gameplay/ProcessGameplayState.cs
+++ StickmanSliding.Unity/Assets/Scripts/Runtime/Architecture/GameStates/Gameplay/ProcessGameplayState.cs
@@ -1,4 +1,5 @@
-﻿using StickmanSliding.Features.Player;
+﻿using StickmanSliding.Features.Camera;
+using StickmanSliding.Features.Player;
 using StickmanSliding.Infrastructure.InputServices;
 using StickmanSliding.Utilities.Patterns.State.Types;
 using Zenject;
@@ -7,19 +8,22 @@ namespace StickmanSliding.Architecture.GameStates.Gameplay
 {
     public class ProcessGameplayState : IEnterableState, IExitableState
     {
-        [Inject] private readonly IMoveInputService _moveInputService;
-        [Inject] private readonly IPlayerProvider   _playerProvider;
+        [Inject] private readonly ICameraTargetFollower _cameraTargetFollower;
+        [Inject] private readonly IMoveInputService     _moveInputService;
+        [Inject] private readonly IPlayerProvider       _playerProvider;
 
         public void Enter()
         {
+            _cameraTargetFollower.StartFollowing(_playerProvider.Player.transform);
             _moveInputService.Enable();
             _playerProvider.Player.Mover.StartMoving();
         }
 
         public void Exit()
         {
-            _moveInputService.Disable();
             _playerProvider.Player.Mover.StopMoving();
+            _moveInputService.Disable();
+            _cameraTargetFollower.StopFollowing();
         }
     }
 }
