2b3d229be9a176a6dd8790aeec743153492e8f58
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2026-08-16 16:49:47 +0300

    Finish gameplay after player hits or steps on wall

---
+++ StickmanSliding.Unity/Assets/Scripts/Runtime/Features/ObstacleCube/IPlayerCollisionHandler.cs
@@ -0,0 +1,10 @@
+﻿using StickmanSliding.Features.Player;
+using StickmanSliding.Features.Track;
+
+namespace StickmanSliding.Features.ObstacleCube
+{
+    public interface IPlayerCollisionHandler
+    {
+        void HandleEnter((PlayerEntity Entity, UnityEngine.Collision Collision) player, TrackPartEntity track);
+    }
+}
---
+++ StickmanSliding.Unity/Assets/Scripts/Runtime/Features/ObstacleCube/PlayerCollisionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StickmanSliding.Architecture.GameStates.Gameplay;
+using StickmanSliding.Features.CollectableCube;
+using StickmanSliding.Features.Player;
+using StickmanSliding.Features.Track;
+using Zenject;
+
+namespace StickmanSliding.Features.ObstacleCube
+{
+    public class PlayerCollisionHandler : IPlayerCollisionHandler, IGameplayFinishingInformer
+    {
+        public event Action GameplayFinished;
+
+        [Inject] private readonly IPlayerCubeDetacher _playerCubeDetacher;
+
+        public void HandleEnter((PlayerEntity Entity, UnityEngine.Collision Collision) player, TrackPartEntity track)
+        {
+            var cube = player.Collision.transform.GetComponentInParent<CollectableCubeEntity>();
+
+            if (cube == default)
+                GameplayFinished?.Invoke();
+            else if (_playerCubeDetacher.IsCollisionFromDetachableDirection(player.Collision))
+                _playerCubeDetacher.Detach(player.Entity, cube, track);
+        }
+    }
+}
